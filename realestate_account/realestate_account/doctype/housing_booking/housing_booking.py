14111f631b2b397652658e84ab5e30d0d3157da6
pantcr7 <prasantpant141@gmail.com>
2023-05-08 15:34:35 +0545

    Dashboard files added

--- realestate_account/realestate_account/doctype/housing_booking/housing_booking.py
+++ realestate_account/realestate_account/doctype/housing_booking/housing_booking.py
@@ -2,7 +2,10 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.model.document import Document
+#from frappe.model.document import Document
 
-class HousingBooking(Document):
-	pass
+#class HousingBooking(Document):pass
+	
+	
+data = frappe.db.get_list('Installment', fields=['installment_name','installment_type', 'Operation'],order_by='creation asc')
+frappe.response['Installment_List'] = data
